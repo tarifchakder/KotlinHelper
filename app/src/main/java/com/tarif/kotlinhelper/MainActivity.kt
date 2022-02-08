e1181a1cc13b74f57f6649a173808210f580a0ed
MR Tech <tarifchakder@outlook.com>
2022-02-08 12:29:49 +0530

    fix

--- app/src/main/java/com/tarif/kotlinhelper/MainActivity.kt
+++ app/src/main/java/com/tarif/kotlinhelper/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.tarif.kotlinhelper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tarif.kotlinhelper.databinding.ActivityMainBinding
+import com.tarif.view.showIf
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        bind.root.showIf(true, true)
     }
 }
